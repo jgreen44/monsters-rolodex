02cadc9170a7f8320638f4f1fd600632db367ded
Jason Green <jgreen44@asu.edu>
2022-01-05 16:39:54 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters: [
+        {name: 'Frankenstein'},
+        {name: 'Dracula'},
+        {name: 'Zombie'},
+      ]
+    }
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+              {this.state.string}
+            </p>
+            <button onClick={() => this.setState({string: "Hello button click!"})}>Click Me!</button>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
