635dd114b0a35aa03342580fec6085dddc3ed864
Jason Green <jgreen44@asu.edu>
2023-01-23 14:58:34 -0600

    add name functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
 import './App.css';
 
 import * as React from 'react';
+import { Component } from 'react';
 
 import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      name: 'jason',
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hi {this.state.name}</p>
+          <button
+            onClick={() => {
+              this.setState(
+                () => {
+                  return {
+                    name: { firstName: 'jason', lastName: 'green' },
+                  };
+                },
+                () => {
+                  // eslint-disable-next-line no-console
+                  console.log(this.state);
+                },
+              );
+            }}
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
