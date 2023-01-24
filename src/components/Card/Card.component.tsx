52815e1267b02de175723a636bf766462bc6a888
Jason Green <jgreen44@asu.edu>
2023-01-24 01:38:13 -0600

    syntax fix

--- src/components/Card/Card.component.tsx
+++ src/components/Card/Card.component.tsx
@@ -6,7 +6,7 @@ import { IMonsterData } from './types';
 
 export const Card = ({ id, name, email, phone }: IMonsterData) => {
   return (
-    <div className={'card-container'} data-testid='card'>
+    <div className={'card-container'} data-testid={'card'}>
       <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
       <h2>{name}</h2>
       <h2>{email}</h2>
